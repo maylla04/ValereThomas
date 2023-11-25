aabdb24fddfaf104bc5682b5320ba601e1a64aae
Maylla Nascimento <maylla.rie@gmail.com>
2023-11-24 22:44:21 -0300

    git finish

--- valerethomas/src/app/page.js
+++ valerethomas/src/app/page.js
@@ -1,113 +1,64 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex  flex-col justify-between p-5">
+      <header className="flex border-b-2 px-6 py-2 h-16">
+        <h1 className="font-bold">Valerie Thomas</h1>
+        <div className="grow">
+          <div className="flex items-center justify-center gap-2 md:gap-8">
+            <Link href="home">Home</Link>
+            <Link href="home">About</Link>
+            <Link href="home">Bio</Link>
+            <Link href="home">Contact</Link>
+          </div>
+        </div>
+        <div className="flex justify-center gap-2 md:gap-8">
+          <Link href="register" className="mr-2 font-bold">Sign up</Link>
+          <Link href="login" className="mr-2 font-bold">Login</Link>
+        </div>
+      </header>
+      <center><h1 className="font-bold text-3xl p-8">CONHEÇA VALERIE THOMAS</h1></center>
+      <div className="grid grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2  sm:grid-cols-1 md:grid-cols-1 p-5  mt-5 ml-2 mb-5 mr-2 shadow-xl">
+        
+        <div className="flex flex-col">
+        <h1 className='text-2xl p-5 font-semibold text-justify'>Valerie Thomas: criadora da tecnologia 3D</h1>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fprimeirosnegros.b-cdn.net%2Fwp-content%2Fuploads%2F2021%2F10%2FDESTAQUE_Valerie-1.jpg&f=1&nofb=1&ipt=917d93c1f492b65e565253ee2f0054a8c380a33c0b07eb1fc75a79c1f43b3b98&ipo=images"/>
+        </div>
+        <div className="p-8">
+          <div className="col-span-2  xl:ml-5 2xl:ml-5 mt-5">
+          
+          <p className='p-5 text-justify'>Valerie Thomas (nascida em Maio de 1943) é uma física norte-americana. 
+          Apesar do pouco incentivo quando criança, a curiosidade de Valerie a levou para as ciências e depois de uma 
+          graduação marcada por seu ótimo desempenho, começou a trabalhar na NASA. Em 1976, Thomas assistiu a uma palestra 
+          que a inspirou a fazer experimentos com espelhos. Espelhos retos geram o reflexo de um objeto que parece estar 
+          atrás da superfície do vidro. Já um espelho côncavo mostra um reflexo que parece existir em frente ao vidro, 
+          trazendo a ilusão de que esta imagem existe de forma tridimensional. Thomas pensou que este método seria mais 
+          eficiente para mostrar imagens de vídeo, tanto para televisões quanto para equipamentos científicos, e portanto, 
+          desenvolveu e patenteou o seu transmissor de ilusão, que foi adotado pela NASA e é usado em cirurgias e na produção 
+          de televisões e monitores. Thomas também contribuiu para a astronomia desenvolvendo programas para as pesquisas do 
+          cometa Halley, da camada de ozônio e para tecnologia de satélites. Hoje em dia ela é pesquisadora associada na Universidade de Baltimore e 
+          também faz parte de um programa científico que tutora jovens.</p>
+         <p className='p-5'>Referências: <a className='hover:font-bold ' href='https://aulanapratica.wordpress.com/tag/valerie-thomas/'>Clique aqui</a></p>
+          </div>
         </div>
       </div>
+      <footer className="border-t-2 px-2 py-2 grid grid-cols-1  justify-between">
+        <div className="p-5">
+          <h1 className="font-semibold mb-5 ">Contato</h1>
+          <a href="https://www.linkedin.com/in/maylla-nascimento-83a67a266/" ><p className="hover:font-semibold">Linkedin</p></a>
+          
+          <a href="maylla.nascimento@estudante.ifms.edu.br" ><p className="hover:font-semibold">Email: maylla.rie@gmail.com</p></a>
+          <p className="">Feito por: Maylla Nascimento</p>
+         
+        </div>
+        
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        
+        
+        
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </footer>
     </main>
   )
 }
